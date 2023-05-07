10a94b41bc0c5ca42fccfe31e4dc1c45c2fed068
Your Namedavid-taiwo <davidtaiwo003@gmail.com>
2023-05-07 22:53:58 +0100

    adding starting project

---
+++ app.js
@@ -0,0 +1,56 @@
+// BUDGET CONTROLLER
+var budgetController = (function() {
+
+
+    
+})();
+
+// THE UI CONTROLLER MODULE
+var UIController = (function() {
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value'
+    }
+
+   return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        }
+        
+   };
+
+})();
+
+// GLOBAL APP CONTROLLER MODULE (this is to link the UI and Budget module together by passing the other two modules as argument)
+var controller = (function(budgetCtrl, UICtrl) {
+
+    var ctrlAddItem = function() {
+        // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
+
+        // 2. Add the item to the budget controller
+
+        // 3. Add the item to the UI
+
+        // 4. Calculate the budget
+
+        // 5. Display the budget on the UI
+    }
+
+
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+    
+})(budgetController, UIController);
